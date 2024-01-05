6e8633700b40201bf463d0fb1b73546d3e419c56
Varun Chopra <varunchopra2003@gmail.com>
2024-01-05 17:45:06 +0530

    error solved

--- inference.py
+++ inference.py
@@ -98,7 +98,7 @@ def run_face_mp(image):
                 mp_drawing.draw_landmarks(
                     image=image,
                     landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACE_CONNECTIONS,
+                    connections=mp_face_mesh.FACEMESH_TESSELATION,
                     landmark_drawing_spec=drawing_spec,
                     connection_drawing_spec=drawing_spec)
 
